b192fd1dc86d648051b2c9f42ecac089eb337a7a
domenicobacoli <domenico.bacoli89@gmail.com>
2023-03-01 16:54:45 +0100

    init esercizio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*
+Consegna:
+Data una lista della spesa (memorizzata in un array), 
+stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi 
+della lista individualmente con un ciclo while.
+*/
+
+
